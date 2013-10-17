854a3f3dce2308a6badacff431db7ec5633c0e3b
XATAU <LEXA-Lizin@yandex.ru>
2013-10-18 00:46:50 +0400

    1

---
+++ Новая папка (4)/test/test/Main.cs
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+using System;
+
+namespace test
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Лабораторная работа #1, Сервис обмена сообщениями");
+			StartServer();
+
+		}
+		public static void StartServer ()
+		{
+			Console.WriteLine("Сервер запущен");
+			new Server(1235);		
+			//Console.WriteLine("Сервер остановлен");
+		}
+	}
+}
+=======
+using System;
+
+namespace test
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Лабораторная работа #1, Сервис обмена сообщениями");
+			StartServer();
+
+		}
+		public static void StartServer ()
+		{
+			Console.WriteLine("Сервер запущен");
+			new Server(1235);		
+		}
+	}
+}
+>>>>>>> adb03089d6c3267ad52c5c77f20ff16070f3ea95
---
+++ Новая папка (4)/test/test/Message.cs
@@ -0,0 +1,69 @@
+<<<<<<< HEAD
+using System;
+
+namespace test
+{
+	public class Message
+	{
+
+		private String from;
+		private String to;
+		private String message;
+		private bool check;
+
+		public Message (String From,String To,String Message){
+			from=From;
+			to=To;
+			message=Message;
+			check=false;
+		}
+        public String getto()
+        {
+            return to;
+        }
+        public String getmessage()
+        {
+            return message;
+        }
+        public String getfrom()
+        {
+            return from;
+        }
+	}
+}
+
+=======
+using System;
+
+namespace test
+{
+	public class Message
+	{
+
+		private String from;
+		private String to;
+		private String message;
+		private bool check;
+
+		public Message (String From,String To,String Message){
+			from=From;
+			to=To;
+			message=Message;
+			check=false;
+		}
+        public String getto()
+        {
+            return to;
+        }
+        public String getmessage()
+        {
+            return message;
+        }
+        public String getfrom()
+        {
+            return from;
+        }
+	}
+}
+
+>>>>>>> adb03089d6c3267ad52c5c77f20ff16070f3ea95
